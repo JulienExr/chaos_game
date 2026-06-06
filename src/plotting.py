39242c934003f386ec5df73d3634648f779d20b9
Julien <julien.excoffier@ecole.ensicaen.fr>
2026-06-07 01:09:57 +0200

    add smoothier vertex plot

--- src/plotting.py
+++ src/plotting.py
@@ -38,9 +38,22 @@ def draw_fractal(
         ax.scatter(visible_points[:, 0], visible_points[:, 1], s=0.1, c="black")
 
     if show_vertices:
-        ax.scatter(vertices[:, 0], vertices[:, 1], s=55, c=colors, edgecolors="black")
-        for i, vertex in enumerate(vertices):
-            ax.text(vertex[0], vertex[1], str(i), fontsize=10)
+        ax.scatter(
+            vertices[:, 0],
+            vertices[:, 1],
+            s=26,
+            facecolors="none",
+            edgecolors=colors,
+            linewidths=1.2,
+            alpha=0.75,
+        )
+        ax.plot(
+            np.append(vertices[:, 0], vertices[0, 0]),
+            np.append(vertices[:, 1], vertices[0, 1]),
+            color="0.25",
+            linewidth=0.8,
+            alpha=0.18,
+        )
 
     ax.set_aspect("equal")
     ax.axis("off")
